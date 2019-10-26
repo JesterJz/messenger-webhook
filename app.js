cfb08eb1f5191b01c579569fae18c415a89afa9c
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-10-26 12:53:45 +0700

    console add

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.get('/', (req, res) => {
-  res.send("Home page. Server running okay.");
+  console.log("Home page. Server running okay.");
 });
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
