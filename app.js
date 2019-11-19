e669dbed3cd519df5dd5acdaa7656d2bc9816fe3
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-11-19 23:33:46 +0700

    a

--- app.js
+++ app.js
@@ -88,9 +88,9 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    $mess = getapiserver(sender_psid);
+    // $mess = getapiserver(sender_psid);
     response = {
-      "text": `You sent the message: "${mess.text}". Now send me an attachment!`
+      "text": `You sent the message: "${received_message}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
