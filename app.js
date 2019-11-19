fb102e9a8293b3b9527f55c63182e1972e2cffbc
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-11-19 23:34:59 +0700

    a

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function handleMessage(sender_psid, received_message) {
     // will be added to the body of our request to the Send API
     // $mess = getapiserver(sender_psid);
     response = {
-      "text": `You sent the message: "${received_message}". Now send me an attachment!`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
