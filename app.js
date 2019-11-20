9bc845e37768b69476456fea68e6a121239dd4bf
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-11-20 09:08:10 +0700

    test

--- app.js
+++ app.js
@@ -93,15 +93,10 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    request('http://localhost/Jester/public/api/hello', function (error, responsea, body) {
-    var person = JSON.parse(body);
-    response = person[0].title;
-    //console.log(person[0].title); // Print the HTML for the Google homepage.
-});
-    // response = {
+    response = {
 
-    //   "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    // }
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
