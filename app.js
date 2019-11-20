a5385426dda5129e7dd5ea0227d0038ccaf895a6
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-11-20 09:18:22 +0700

    test

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function handleMessage(sender_psid, received_message) {
     // will be added to the body of our request to the Send API
     response = {
 
-      "text": `có con cặt đây`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
