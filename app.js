dfc21ddb2fc2d2ff9fd71832228eecb6b402c22c
Thanh0ajj6 <zimmer.jeyy@gmail.com>
2019-11-25 09:56:47 +0700

    test n

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ function handleMessage(sender_psid, received_message) {
     //   response = {
     //   "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     // }
-    callgetapi(sender_psid, received_message.text);
+    callgetapi(sender_psid);
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
